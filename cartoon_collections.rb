7e90e46358a6eb3e23b4bc74ffb3764431650f93
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 02:53:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-seven_dwarves = [ "Grumpy", "Doc", "Happy", "Sneezy", "Bashful", "Dopey", "Sleepy"]  # Your code here
+seven_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]  # Your code here
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +14,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+#"Grumpy", "Doc", "Happy", "Sneezy", "Bashful", "Dopey", "Sleepy"
