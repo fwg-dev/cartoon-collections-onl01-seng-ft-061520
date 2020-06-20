9b37fa8d03b0c6192327b64efede51bfcdf89423
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 03:41:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  call.length > 4 
-  
-  
+  call.length > 4
+
+
 end
 
 def find_the_cheese# code an argument here
