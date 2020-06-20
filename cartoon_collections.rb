53a834b50cdd6038aed843a28514bc8672f36d23
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 02:51:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+seven_dwarves = [ "Grumpy", "Doc", "Happy", "Sneezy", "Bashful", "Dopey", "Sleepy"]  # Your code here
 end
 
 def summon_captain_planet# code an argument here
