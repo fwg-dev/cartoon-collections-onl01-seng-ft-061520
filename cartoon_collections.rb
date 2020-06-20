af28ab86df5ecf35a1e4f66f29de8a155c5dbca4
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 03:39:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer)# code an argument here
   # Your code here
   planeteer.collect do |call|
     call.capitalize << "!"
-  
+
 end
 
 def long_planeteer_calls# code an argument here
